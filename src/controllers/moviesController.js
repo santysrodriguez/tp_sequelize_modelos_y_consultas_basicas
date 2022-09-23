9fba4c657989b226c65e7e5f15286ef2d6411a0a
santysrodriguez <santiagocarp987@gmail.com>
2022-09-22 21:01:02 -0300

    advances

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,16 @@
+const db = require('../database/models')
+
+module.exports = {
+    list : (req,res) => {
+
+    },
+    new : (req,res) => {
+
+    },
+    recommended : (req,res) => {
+
+    },
+    detail : (req,res) => {
+        
+    }
+}
